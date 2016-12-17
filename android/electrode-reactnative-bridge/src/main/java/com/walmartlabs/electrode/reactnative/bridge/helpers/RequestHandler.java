99cbf52bdf613acdaa00be2631d241b2c6f9c5d2
Benoit Lemaire <BLemaire@walmartlabs.com>
2016-12-16 19:32:45 -0800

    [Android] Add RequestHandlerEx helper interface. Bump AAR version to 0.0.5

--- android/electrode-reactnative-bridge/src/main/java/com/walmartlabs/electrode/reactnative/bridge/helpers/RequestHandler.java
+++ android/electrode-reactnative-bridge/src/main/java/com/walmartlabs/electrode/reactnative/bridge/helpers/RequestHandler.java
@@ -1,5 +1,9 @@
 package com.walmartlabs.electrode.reactnative.bridge.helpers;
 
-public interface RequestHandler<T> {
-  void handleRequest(Response<T> response);
+/**
+ * Basic RequestHandler taking no request payload and returning a response
+ * @param <TRsp> The response payload type
+ */
+public interface RequestHandler<TRsp> {
+  void handleRequest(Response<TRsp> response);
 }

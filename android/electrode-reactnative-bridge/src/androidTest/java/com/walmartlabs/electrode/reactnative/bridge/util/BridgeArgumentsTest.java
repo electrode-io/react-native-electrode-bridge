a11c70726aed6e2a0bd119f4ccd90ad9e5021356
Deepu Eswarath Ganapathiyadan <DGanapathiyadan@walmartlabs.com>
2017-02-22 16:22:28 -0800

    add helper methods to convert objects to bundle and vice versa.

---
+++ android/electrode-reactnative-bridge/src/androidTest/java/com/walmartlabs/electrode/reactnative/bridge/util/BridgeArgumentsTest.java
@@ -0,0 +1,62 @@
+package com.walmartlabs.electrode.reactnative.bridge.util;
+
+import android.os.Bundle;
+
+import com.walmartlabs.electrode.reactnative.bridge.helpers.Logger;
+import com.walmartlabs.electrode.reactnative.sample.model.BirthYear;
+import com.walmartlabs.electrode.reactnative.sample.model.Person;
+import com.walmartlabs.electrode.reactnative.sample.model.Position;
+import com.walmartlabs.electrode.reactnative.sample.model.Status;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by d0g00g4 on 2/21/17.
+ */
+public class BridgeArgumentsTest extends TestCase {
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        Logger.overrideLogLevel(Logger.LogLevel.DEBUG);
+    }
+
+    public void testFromBundleSuccess() {
+        Person person = new Person.Builder("Deepu", 10).build();
+        Bundle personBundle = person.toBundle();
+
+        Person personCopy = BridgeArguments.bridgeableFromBundle(personBundle, Person.class);
+        assertNotNull(personCopy);
+        assertEquals(person.getName(), personCopy.getName());
+        assertEquals(person.getMonth(), personCopy.getMonth());
+    }
+
+    public void testFromBundleSuccess1() {
+        Status status = new Status.Builder(true).log(false).build();
+        BirthYear birthYear = new BirthYear.Builder(01, 2000).build();
+        Position position = new Position.Builder().build();
+
+        Person person = new Person.Builder("Richard", 10)
+                .age(18)
+                .birthYear(birthYear)
+                .status(status)
+                .position(position)
+                .build();
+        Bundle personBundle = person.toBundle();
+
+        Person personCopy = BridgeArguments.bridgeableFromBundle(personBundle, Person.class);
+        assertNotNull(personCopy);
+        assertEquals(person.getName(), personCopy.getName());
+        assertEquals(person.getMonth(), personCopy.getMonth());
+        assertNotNull(person.getStatus());
+        assertEquals(person.getStatus().getLog(), personCopy.getStatus().getLog());
+        assertEquals(person.getStatus().getMember(), personCopy.getStatus().getMember());
+        assertNotNull(person.getBirthYear());
+        assertNotNull(person.getPosition());
+    }
+
+    public void testFromBundleWithEmptyBundle() {
+        Person personCopy = BridgeArguments.bridgeableFromBundle(Bundle.EMPTY, Person.class);
+        assertNull(personCopy);
+    }
+}
--- android/electrode-reactnative-bridge/src/main/java/com/walmartlabs/electrode/reactnative/bridge/util/BridgeArguments.java
+++ android/electrode-reactnative-bridge/src/main/java/com/walmartlabs/electrode/reactnative/bridge/util/BridgeArguments.java
@@ -2,16 +2,24 @@ package com.walmartlabs.electrode.reactnative.bridge.util;
 
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 
 import com.facebook.react.bridge.ReadableArray;
 import com.facebook.react.bridge.ReadableMap;
+import com.walmartlabs.electrode.reactnative.bridge.Bridgeable;
 import com.walmartlabs.electrode.reactnative.bridge.helpers.ArgumentsEx;
+import com.walmartlabs.electrode.reactnative.bridge.helpers.Logger;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
 
 /**
  * This class contains utility methods for bridge
  */
 
 public class BridgeArguments {
+
+    private static final String TAG = BridgeArguments.class.getSimpleName();
 
     /**
      * @param data    {@link ReadableMap} received from JS side.
@@ -64,6 +72,81 @@ public class BridgeArguments {
                 case Null:
                     break;
             }
+        }
+        return bundle;
+    }
+
+    @Nullable
+    public static <T> T bridgeableFromBundle(@NonNull Bundle bundle, @NonNull Class<T> clazz) {
+        Logger.d(TAG, "entering bridgeableFromBundle with bundle(%s) for class(%s)", bundle, clazz);
+
+        if (!bundle.containsKey(Bridgeable.KEY_BUNDLE_ID)
+                || !(clazz.getSimpleName().equals(bundle.getString(Bridgeable.KEY_BUNDLE_ID)))) {
+            Logger.w(TAG, "Looks like the given bundle(%s) does not include KEY_BUNDLE_ID entry, expected(%s) != actual(%s)", bundle, clazz.getSimpleName(), bundle.getString(Bridgeable.KEY_BUNDLE_ID));
+            return null;
+        }
+
+        try {
+            Class clz = Class.forName(clazz.getName());
+            Constructor[] constructors = clz.getDeclaredConstructors();
+            for (Constructor constructor : constructors) {
+                if (constructor.getParameterTypes().length == 1) {
+                    if (constructor.getParameterTypes()[0].isInstance(bundle)) {
+                        Object[] args = new Object[1];
+                        args[0] = bundle;
+                        Object result = constructor.newInstance(args);
+                        if (clazz.isInstance(result)) {
+                            return (T) result;
+                        } else {
+                            Logger.w(TAG, "Object creation from bundle not possible since the created object(%s) is not an instance of %s", result, clazz);
+                        }
+                    }
+                    //Empty constructor available, object construction using bundle can now be attempted.
+                    break;
+                }
+            }
+            Logger.w(TAG, "Could not find a constructor that takes in a Bundle param for class(%s)", clazz);
+        } catch (ClassNotFoundException e) {
+            Logger.w(TAG, "FromBundle failed to execute(%s)", e.getCause());
+        } catch (InstantiationException e) {
+            Logger.w(TAG, "FromBundle failed to execute(%s)", e.getCause());
+        } catch (IllegalAccessException e) {
+            Logger.w(TAG, "FromBundle failed to execute(%s)", e.getCause());
+        } catch (InvocationTargetException e) {
+            Logger.w(TAG, "FromBundle failed to execute(%s)", e.getCause());
+        }
+
+        Logger.d(TAG, "FromBundle failed to execute");
+        return null;
+    }
+
+    public static Object getPrimitiveFromBundleForRequest(@NonNull Bundle payload, @NonNull Class reqClazz) {
+        Object value = null;
+        if (String.class.isAssignableFrom(reqClazz)) {
+            value = payload.getString("req");
+        } else if (Integer.class.isAssignableFrom(reqClazz)) {
+            value = payload.getInt("req");
+        } else if (Boolean.class.isAssignableFrom(reqClazz)) {
+            value = payload.getBoolean("req");
+        }
+
+        if (reqClazz.isInstance(value)) {
+            return value;
+        } else {
+            throw new IllegalArgumentException("Should never happen, looks like logic to handle " + reqClazz + " is not implemented yet");
+        }
+    }
+
+    public static Bundle getBundleFromPrimitiveForResponse(@NonNull Object respObj, @NonNull Class respClass) {
+        Bundle bundle = new Bundle();
+        if (String.class.isAssignableFrom(respClass)) {
+            bundle.putString("rsp", (String) respObj);
+        } else if (Integer.class.isAssignableFrom(respClass)) {
+            bundle.putInt("rsp", (Integer) respObj);
+        } else if (Boolean.class.isAssignableFrom(respClass)) {
+            bundle.putBoolean("rsp", (Boolean) respObj);
+        } else {
+            throw new IllegalArgumentException("Should never happen, looks like logic to handle " + respClass + " is not implemented yet");
         }
         return bundle;
     }

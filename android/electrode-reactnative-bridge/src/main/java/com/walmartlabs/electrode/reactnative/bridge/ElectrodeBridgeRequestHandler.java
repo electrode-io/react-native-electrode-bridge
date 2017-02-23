40e5851db70b395836b27ba8487ff698554c63a4
Deepu Eswarath Ganapathiyadan <DGanapathiyadan@walmartlabs.com>
2017-02-22 16:21:27 -0800

    Apply generics to request handler to specify the request and response types.!

--- android/electrode-reactnative-bridge/src/main/java/com/walmartlabs/electrode/reactnative/bridge/ElectrodeBridgeRequestHandler.java
+++ android/electrode-reactnative-bridge/src/main/java/com/walmartlabs/electrode/reactnative/bridge/ElectrodeBridgeRequestHandler.java
@@ -1,17 +1,18 @@
 package com.walmartlabs.electrode.reactnative.bridge;
 
-import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 
 /**
- * Provide method to be notified of incoming request. An implementor of this interface is expected to handle any incoming request and provide a success or failure response.
+ * Provide method to be notified of incoming request.
+ * An implementor of this interface is expected to handle any incoming request and provide a {@link ElectrodeBridgeResponseListener#onSuccess(Object)} or {@link ElectrodeBridgeResponseListener#onFailure(FailureMessage)} response.
  */
-public interface ElectrodeBridgeRequestHandler {
+public interface ElectrodeBridgeRequestHandler<TReq, TResp> {
     /**
      * Called whenever a request matching this handler is received
      *
-     * @param payload             The payload of the request as a Bundle
-     * @param requestCompletioner An instance of RequestCompletioner
+     * @param payload          The payload of the request as a Bundle, payload can be null for a request.
+     * @param responseListener An instance of {@link ElectrodeBridgeResponseListener}
      */
-    void onRequest(@NonNull Bundle payload, @NonNull ElectrodeBridgeResponseListener requestCompletioner);
+    void onRequest(@Nullable TReq payload, @NonNull ElectrodeBridgeResponseListener<TResp> responseListener);
 }

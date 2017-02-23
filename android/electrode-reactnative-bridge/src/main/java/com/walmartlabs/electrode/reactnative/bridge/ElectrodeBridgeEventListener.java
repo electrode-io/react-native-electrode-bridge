d37c7654499da1bc36bfe438a19ee6303db10afb
Deepu Eswarath Ganapathiyadan <DGanapathiyadan@walmartlabs.com>
2017-02-23 11:03:02 -0800

    event response can be null

--- android/electrode-reactnative-bridge/src/main/java/com/walmartlabs/electrode/reactnative/bridge/ElectrodeBridgeEventListener.java
+++ android/electrode-reactnative-bridge/src/main/java/com/walmartlabs/electrode/reactnative/bridge/ElectrodeBridgeEventListener.java
@@ -1,6 +1,7 @@
 package com.walmartlabs.electrode.reactnative.bridge;
 
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 
 /**
  * Provide method to be notified of incoming event
@@ -11,5 +12,5 @@ public interface ElectrodeBridgeEventListener<T> {
      *
      * @param eventPayload The event payload
      */
-    void onEvent(@NonNull T eventPayload);
+    void onEvent(@Nullable T eventPayload);
 }

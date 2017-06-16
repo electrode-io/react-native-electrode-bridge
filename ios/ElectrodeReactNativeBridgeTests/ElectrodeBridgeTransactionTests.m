4051059ceef1a675f060cfbee46d972f8d75974d
Bharath M <bmarulasiddappa@walmartlabs.com>
2017-06-16 15:39:26 -0700

    transaction test refactor

--- ios/ElectrodeReactNativeBridgeTests/ElectrodeBridgeTransactionTests.m
+++ ios/ElectrodeReactNativeBridgeTests/ElectrodeBridgeTransactionTests.m
@@ -1,44 +1,42 @@
-////
-////  ElectrodeBridgeTransactionTests.m
-////  ElectrodeReactNativeBridge
-////
-////  Created by Bharath Marulasiddappa on 3/30/17.
-////  Copyright © 2017 Walmart. All rights reserved.
-////
 //
-//#import <XCTest/XCTest.h>
-//#import "ElectrodeBridgeMessage.h"
-//#import "ElectrodeBridgeTransaction.h"
-//#import "ElectrodeBridgeRequestNew.h"
-//#import "ElectrodeBridgeProtocols.h"
+//  ElectrodeBridgeTransactionTests.m
+//  ElectrodeReactNativeBridge
 //
+//  Created by Bharath Marulasiddappa on 3/30/17.
+//  Copyright © 2017 Walmart. All rights reserved.
 //
-//@interface MockElectrodeBridgeResponseListener : NSObject <ElectrodeBridgeResponseListener>
-//@end
-//
-//@interface ElectrodeBridgeTransactionTests : XCTestCase
-//@end
-//
-//@implementation ElectrodeBridgeTransactionTests
-//- (void)testBridgeTransaction {
-//    NSString* messageName = @"com.walmart.ern.requestmessage";
-//    NSDictionary* messageData = @{@"test" : @"requestMessage"};
-//    NSString* messageId = [ElectrodeBridgeMessage UUID];
-//    ElectrodeMessageType messageType = ElectrodeMessageTypeRequest;
-//    NSDictionary* data = @{
-//                           kElectrodeBridgeMessageName : messageName,
-//                           kElectrodeBridgeMessageId : messageId,
-//                           kElectrodeBridgeMessageType : [ElectrodeBridgeMessage convertEnumTypeToString:messageType] ,
-//                           kElectrodeBridgeMessageData : messageData
-//                           };
-//    ElectrodeBridgeRequestNew* requestMessage = [ElectrodeBridgeRequestNew createRequestWithData:data];
-//    XCTAssertNotNil(requestMessage, @"request is not created");
-//    MockElectrodeBridgeResponseListener* responseListener = [[MockElectrodeBridgeResponseListener alloc] init];
-//    ElectrodeBridgeTransaction* transaction = [[ElectrodeBridgeTransaction alloc] initWithRequest:requestMessage responseListener:responseListener];
-//    XCTAssertNotNil(transaction, @"transaction is not created");
-//    XCTAssertEqual(transaction.finalResponseListener, responseListener, @"invalid assignment");
-//    XCTAssertEqual(transaction.request, requestMessage, @"invalid assignment");
-//    XCTAssertEqual(transaction.transactionId, requestMessage.messageId, @"invalid assignment");
-//}
-//
-//@end
+
+#import <XCTest/XCTest.h>
+#import "ElectrodeBridgeMessage.h"
+#import "ElectrodeBridgeTransaction.h"
+#import "ElectrodeBridgeRequest.h"
+#import "ElectrodeBridgeProtocols.h"
+
+
+@interface ElectrodeBridgeTransactionTests : XCTestCase
+@end
+
+@implementation ElectrodeBridgeTransactionTests
+
+- (void)testTransactionInstance {
+    NSString* messageName = @"com.walmart.ern.requestmessage";
+    NSDictionary* messageData = @{@"test" : @"requestMessage"};
+    NSString* messageId = [ElectrodeBridgeMessage UUID];
+    ElectrodeMessageType messageType = ElectrodeMessageTypeRequest;
+    NSDictionary* data = @{
+                           kElectrodeBridgeMessageName : messageName,
+                           kElectrodeBridgeMessageId : messageId,
+                           kElectrodeBridgeMessageType : [ElectrodeBridgeMessage convertEnumTypeToString:messageType] ,
+                           kElectrodeBridgeMessageData : messageData
+                           };
+    ElectrodeBridgeRequest* request = [ElectrodeBridgeRequest createRequestWithData:data];
+    XCTAssertNotNil(request, @"request instance error");
+    ElectrodeBridgeTransaction* transaction = [[ElectrodeBridgeTransaction alloc] initWithRequest:request completionHandler:^(id  _Nullable data, id<ElectrodeFailureMessage>  _Nullable message) {
+    }];
+    XCTAssertNotNil(transaction, @"transaction instance error");
+    XCTAssertNotNil(transaction.completion, @"transaction's completion handler is nil");
+    XCTAssertEqual(transaction.request, request, @"invalid assignment");
+    XCTAssertEqual(transaction.transactionId, request.messageId, @"invalid assignment");
+}
+
+@end
